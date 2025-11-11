5146ff7584b4eb7dc704e0f8468da273e504897a
Rafael Barbosa <rafael@rfbabarbosa.com>
2025-11-11 17:11:32 -0300

    script devcontainer

--- .devcontainer/script.sh
+++ .devcontainer/script.sh
@@ -3,4 +3,4 @@ set -eux
 sudo apt-get update -y 
 npm i serverless@3.39.0 -g
 mkdir -p ~/.aws/
-cp /workspaces/FIAP-Microservices-Serverless/.devcontainer/config ~/.aws/config
+cp /workspaces/FIAP-Platform-Engineering/.devcontainer/config ~/.aws/config
